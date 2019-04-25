9e3e4e93f59b31b817034e515bcade0fdb9d5a0d
shenminjun <shenminjun@qq.com>
2019-04-25 14:18:04 +0800

    Update settings.py

--- huangzhitao/ai-indicator/settings.py
+++ huangzhitao/ai-indicator/settings.py
@@ -5,7 +5,7 @@ __author__ = "HuangZhiTao"
 
 # 数据库链接参数
 MYSQL_CONNECT = {
-    "host": "139.196.98.163",
+    "host": "127.0.0.1",
     "database": "aiindex",
     "user": "root",
     "passwd": "root",
